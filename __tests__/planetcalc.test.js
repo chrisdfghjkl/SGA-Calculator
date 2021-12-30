0fdfd2d1742e0bed89e554dfe06c950014d93b33
chrisdfghjkl <cdepastene@gmail.com>
2021-12-30 00:28:12 -0800

    add test 5 - jupiter

--- __tests__/planetcalc.test.js
+++ __tests__/planetcalc.test.js
@@ -22,7 +22,10 @@ describe('PlanetAge', () => {
     expect(marsTest.mars()).toEqual(17.021276595744681)
   });
 
-
+  test('should return user age on jupiter', () => {
+    const jupiterTest = new PlanetAge(32);
+    expect(jupiterTest.jupiter()).toEqual(2.69814502529511)
+  });
 
 
 
