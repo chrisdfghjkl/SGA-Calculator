c90600d047e6b97ed921e111107baebbae91e9f1
chrisdfghjkl <cdepastene@gmail.com>
2021-12-29 23:37:10 -0800

    adjusted testing to remove set up

--- __tests__/planetcalc.test.js
+++ __tests__/planetcalc.test.js
@@ -2,21 +2,10 @@ import PlanetAge from './../src/js/planetcalc';
 
 describe('PlanetAge', () => {
 
-  let newAge;
-  beforeEach(() => {
-    newAge = new PlanetAge(30)
-  });
-
   test('should create an object for user with inputted age', () => {
+    const newAge = new PlanetAge(30)
     expect(newAge.age).toEqual(30);
   }); 
-
-  test('should return user ages on different planets', () => {
-    expect(newAge.mercuryAge).toEqual(125);
-    expect(newAge.venusAge).toEqual(48.39);
-    expect(newAge.marsAge).toEqual(15.96);
-    expect(newAge.jupiterAge).toEqual(2.53);
-  });
 
 
 
