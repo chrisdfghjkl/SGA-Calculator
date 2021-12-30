e7ed335731589adbdb0a4c419b1c78619520d422
chrisdfghjkl <cdepastene@gmail.com>
2021-12-29 23:45:00 -0800

    add new test 2

--- __tests__/planetcalc.test.js
+++ __tests__/planetcalc.test.js
@@ -3,11 +3,14 @@ import PlanetAge from './../src/js/planetcalc';
 describe('PlanetAge', () => {
 
   test('should create an object for user with inputted age', () => {
-    const newAge = new PlanetAge(30)
+    const newAge = new PlanetAge(30);
     expect(newAge.age).toEqual(30);
   }); 
 
-
+  test('should return user age on mercury', () => {
+    const mercTest = new PlanetAge(30); 
+    expect(mercTest.mercury()).toEqual(125);
+  });
 
 
 
