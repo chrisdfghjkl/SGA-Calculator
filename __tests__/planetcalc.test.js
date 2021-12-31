bd90bc5b75914b40fcaea461897d2881bca8e496
chrisdfghjkl <cdepastene@gmail.com>
2021-12-30 23:45:24 -0800

    dry up code,

--- __tests__/planetcalc.test.js
+++ __tests__/planetcalc.test.js
@@ -7,26 +7,6 @@ describe('PlanetAge', () => {
     expect(newAge.age).toEqual(30);
   }); 
 
-  test('should return user age on mercury', () => {
-    const mercTest = new PlanetAge(32); 
-    expect(mercTest.mercury()).toEqual(133.33);
-  });
-
-  test('should return user age on venus', () => {
-    const venusTest = new PlanetAge(32);
-    expect(venusTest.venus()).toEqual(51.61)
-  });
-
-  test('should return user age on mars', () => {
-    const marsTest = new PlanetAge(32);
-    expect(marsTest.mars()).toEqual(17.02)
-  });
-
-  test('should return user age on jupiter', () => {
-    const jupiterTest = new PlanetAge(32);
-    expect(jupiterTest.jupiter()).toEqual(2.70)
-  });
-
   test('should return user age on on different planets', () => {
     const planetsAge = new PlanetAge(30);
     planetsAge.ageConverter();
@@ -35,5 +15,4 @@ describe('PlanetAge', () => {
     expect(planetsAge.marsAge).toEqual(15.96);
     expect(planetsAge.jupiterAge).toEqual(2.53);
   });
-
 });
--- src/js/planetcalc.js
+++ src/js/planetcalc.js
@@ -3,31 +3,10 @@ export default class PlanetAge {
     this.age = age;
   }
 
-  mercury() {
-    this.mercuryAge = parseFloat((this.age / .24).toFixed(2));
-    return this.mercuryAge;
-  }
-
-  venus() {
-    this.venusAge = parseFloat((this.age / .62).toFixed(2));
-    return this.venusAge;
-  }
-
-  mars() {
-    this.marsAge = parseFloat((this.age / 1.88).toFixed(2));
-    return this.marsAge;
-  }
-
-  jupiter() {
-    this.jupiterAge = parseFloat((this.age / 11.86).toFixed(2));
-    return this.jupiterAge;
-  }
-
   ageConverter() {
     this.mercuryAge = parseFloat((this.age / .24).toFixed(2));
     this.venusAge = parseFloat((this.age / .62).toFixed(2));
     this.marsAge = parseFloat((this.age / 1.88).toFixed(2));
     this.jupiterAge = parseFloat((this.age / 11.86).toFixed(2));
   }
-
 }
