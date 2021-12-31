240faf218b032cc907eaca0e8011d90118b9cdbf
chrisdfghjkl <cdepastene@gmail.com>
2021-12-30 22:26:56 -0800

    set expectations to 2 decimal points for testing

--- __tests__/planetcalc.test.js
+++ __tests__/planetcalc.test.js
@@ -9,22 +9,22 @@ describe('PlanetAge', () => {
 
   test('should return user age on mercury', () => {
     const mercTest = new PlanetAge(32); 
-    expect(mercTest.mercury()).toEqual(133.33333333333334);
+    expect(mercTest.mercury()).toEqual(133.33);
   });
 
   test('should return user age on venus', () => {
     const venusTest = new PlanetAge(32);
-    expect(venusTest.venus()).toEqual(51.612903225806452)
+    expect(venusTest.venus()).toEqual(51.61)
   });
 
   test('should return user age on mars', () => {
     const marsTest = new PlanetAge(32);
-    expect(marsTest.mars()).toEqual(17.021276595744681)
+    expect(marsTest.mars()).toEqual(17.02)
   });
 
   test('should return user age on jupiter', () => {
     const jupiterTest = new PlanetAge(32);
-    expect(jupiterTest.jupiter()).toEqual(2.69814502529511)
+    expect(jupiterTest.jupiter()).toEqual(2.70)
   });
 
 
