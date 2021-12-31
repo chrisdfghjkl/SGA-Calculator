ab2f4942e18b2163a6aff4f539688d0935a4d559
chrisdfghjkl <cdepastene@gmail.com>
2021-12-30 23:43:56 -0800

    add passing business logic for test 6

--- src/js/planetcalc.js
+++ src/js/planetcalc.js
@@ -23,4 +23,11 @@ export default class PlanetAge {
     return this.jupiterAge;
   }
 
+  ageConverter() {
+    this.mercuryAge = parseFloat((this.age / .24).toFixed(2));
+    this.venusAge = parseFloat((this.age / .62).toFixed(2));
+    this.marsAge = parseFloat((this.age / 1.88).toFixed(2));
+    this.jupiterAge = parseFloat((this.age / 11.86).toFixed(2));
+  }
+
 }
