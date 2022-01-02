4cb1f6ae9b97a4879daec5f0195a73f25e811e8d
chrisdfghjkl <cdepastene@gmail.com>
2022-01-01 22:58:27 -0800

    add 7th test

--- __tests__/planetcalc.test.js
+++ __tests__/planetcalc.test.js
@@ -15,4 +15,13 @@ describe('PlanetAge', () => {
     expect(planetsAge.marsAge).toEqual(15.96);
     expect(planetsAge.jupiterAge).toEqual(2.53);
   });
+
+  test('should calculate life expectancy on each planet', () =>{
+    const lifespan = new planetsAge(30);
+    lifespan.planetExpectancy();
+    expect(lifespan.mercuryExpectancy).toEqual(302.50);
+    expect(lifespan.venusExpectancy).toEqual(117.10);
+    expect(lifespan.marsExpectancy).toEqual(38.62);
+    expect(lifespan.jupiterExpectancy).toEqual(6.12);
+  });
 });
